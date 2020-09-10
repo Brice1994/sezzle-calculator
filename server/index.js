eb9d3d936ca599e9c28885fafbec38f033997d3c
blehew <blehew@lifetouch.com>
2020-09-10 13:29:10 -0500

    comment

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const router = express.Router();
 
 const dbRoute =
     `mongodb://${process.env.USER}:${process.env.AUTHKEY}@sezzle-web-calculator-shard-00-00.urh1h.mongodb.net:27017,sezzle-web-calculator-shard-00-01.urh1h.mongodb.net:27017,sezzle-web-calculator-shard-00-02.urh1h.mongodb.net:27017/expressions?ssl=true&replicaSet=atlas-qic6uj-shard-0&authSource=admin&retryWrites=true&w=majority`;
-console.log(dbRoute);
+console.log(`Route: ${dbRoute}`);
 mongoose.connect(dbRoute, {
   useNewUrlParser: true,
   useFindAndModify: true,
