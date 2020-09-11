5072d5bbcb86476a21693e8fecde0cd4978bbd4d
Brice1994 <bricelehew@gmail.com>
2020-09-11 06:45:37 -0500

    use env variable for db access

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 app.use(cors());
 
 const dbRoute =
-    `mongodb://user:E3jMs33OymDkdoIG@sezzle-web-calculator-shard-00-00.urh1h.mongodb.net:27017,sezzle-web-calculator-shard-00-01.urh1h.mongodb.net:27017,sezzle-web-calculator-shard-00-02.urh1h.mongodb.net:27017/expressions?ssl=true&replicaSet=atlas-qic6uj-shard-0&authSource=admin&retryWrites=true&w=majority`;
+    `mongodb://${process.env.USER}:${process.env.AUTHKEY}@sezzle-web-calculator-shard-00-00.urh1h.mongodb.net:27017,sezzle-web-calculator-shard-00-01.urh1h.mongodb.net:27017,sezzle-web-calculator-shard-00-02.urh1h.mongodb.net:27017/expressions?ssl=true&replicaSet=atlas-qic6uj-shard-0&authSource=admin&retryWrites=true&w=majority`;
 console.log(`Route: ${dbRoute}`);
 mongoose.connect(dbRoute, {
   useNewUrlParser: true,
